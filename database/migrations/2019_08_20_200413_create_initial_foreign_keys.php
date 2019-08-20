5a9e5e567b52972c862be5aed1ec639375259c64
poppabear8883 <servnx@gmail.com>
2019-08-20 16:27:11 -0400

    (Add) Initial Setup of foreign keys

---
+++ database/migrations/2019_08_20_200413_create_initial_foreign_keys.php
@@ -0,0 +1,75 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInitialForeignKeys extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->foreign('status_id')->references('id')->on('statuses');
+      $table->foreign('server_id')->references('id')->on('servers');
+    });
+
+    Schema::table('websites', function (Blueprint $table) {
+      $table->foreign('domain_id')->references('id')->on('domains');
+      $table->foreign('template_id')->references('id')->on('templates');
+    });
+
+    Schema::table('dns', function (Blueprint $table) {
+      $table->foreign('domain_id')->references('id')->on('domains');
+      $table->foreign('template_id')->references('id')->on('templates');
+    });
+
+    Schema::table('mails', function (Blueprint $table) {
+      $table->foreign('domain_id')->references('id')->on('domains');
+    });
+
+    Schema::table('databases', function (Blueprint $table) {
+      $table->foreign('user_id')->references('id')->on('users');
+    });
+
+    Schema::table('firewalls', function (Blueprint $table) {
+      $table->foreign('server_id')->references('id')->on('servers');
+    });
+
+    Schema::table('crons', function (Blueprint $table) {
+      $table->foreign('user_id')->references('id')->on('users');
+    });
+
+    Schema::table('backups', function (Blueprint $table) {
+      $table->foreign('user_id')->references('id')->on('users');
+    });
+
+    Schema::table('domains', function (Blueprint $table) {
+      $table->foreign('user_id')->references('id')->on('users');
+    });
+
+    Schema::table('package_user', function (Blueprint $table) {
+      $table->foreign('user_id')->references('id')->on('users');
+      $table->foreign('package_id')->references('id')->on('packages');
+    });
+
+    Schema::table('templates', function (Blueprint $table) {
+      $table->foreign('server_id')->references('id')->on('servers');
+      $table->foreign('template_type_id')->references('id')->on('template_types');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    //
+  }
+}
