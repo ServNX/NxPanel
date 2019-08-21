f9b618face0d7e1dcce45d8e95fc6710dce5180c
poppabear8883 <servnx@gmail.com>
2019-08-21 15:45:48 -0400

    (Add) Service and Ip models with migrations

---
+++ database/migrations/2019_08_21_164753_create_ips_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIpsTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    /* IPs belongsTo Server */
+    /* Server hasMany IPs */
+    Schema::create('ips', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->bigInteger('server_id');
+      $table->string('value');
+      $table->timestamps();
+    });
+
+    Schema::table('ips', function (Blueprint $table) {
+      $table->foreign('server_id')->references('id')->on('servers');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('ips');
+  }
+}
