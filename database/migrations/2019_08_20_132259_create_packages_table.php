b4709e379fc74992b9e3d6abcc0a1538cafda666
poppabear8883 <servnx@gmail.com>
2019-08-21 15:43:29 -0400

    [Packages](Add) Additional columns to migration and relationships

--- database/migrations/2019_08_20_132259_create_packages_table.php
+++ database/migrations/2019_08_20_132259_create_packages_table.php
@@ -15,7 +15,37 @@ class CreatePackagesTable extends Migration
     {
         Schema::create('packages', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('web_template_id')->unsigned();
+            $table->bigInteger('dns_template_id')->unsigned();
+            $table->bigInteger('backend_template_id')->unsigned();
+            $table->string('shell')->default('no-login');
+
+            // no plans on supporting more than 4 Nameservers
+            $table->string('ns_one')->default('ns1.domain.tld');
+            $table->string('ns_two')->default('ns2.domain.tld');
+            $table->string('ns_three')->default('ns3.domain.tld');
+            $table->string('ns_four')->default('ns4.domain.tld');
+
+            // 0 = unlimited
+            $table->integer('disk_quota')->default(200); // Mb
+            $table->bigInteger('bandwidth')->default(1000); // Mb
+            $table->integer('web_domains')->default(1);
+            $table->integer('web_aliases')->default(1);
+            $table->integer('dns_domains')->default(1);
+            $table->integer('dns_records')->default(1);
+            $table->integer('mail_domains')->default(1);
+            $table->integer('mail_accounts')->default(1);
+            $table->integer('databases')->default(1);
+            $table->integer('crons')->default(1);
+            $table->integer('backups')->default(1);
+
             $table->timestamps();
+        });
+
+        Schema::table('packages', function (Blueprint $table) {
+            $table->foreign('web_template_id')->references('id')->on('templates');
+            $table->foreign('dns_template_id')->references('id')->on('templates');
+            $table->foreign('backend_template_id')->references('id')->on('templates');
         });
     }
 
