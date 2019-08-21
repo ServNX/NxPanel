cf05c90fb73251c84324e8d0410c7d27b2de57ae
poppabear8883 <servnx@gmail.com>
2019-08-21 15:45:48 -0400

    (Add) Service and Ip models with migrations

---
+++ database/migrations/2019_08_21_170041_create_services_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    /* Service belongsTo Server */
+    /* Server hasMany Services */
+    /* Service belongsTo Status */
+    Schema::create('services', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->bigInteger('server_id');
+      $table->integer('status_id');
+      $table->string('name');
+      $table->string('start');
+      $table->string('restart');
+      $table->string('stop');
+      $table->integer('cpu')->default(0);
+      $table->integer('memory')->default(0);
+      $table->integer('uptime')->default(0);
+      $table->timestamps();
+    });
+
+    Schema::table('services', function (Blueprint $table) {
+      $table->foreign('server_id')->references('id')->on('servers');
+      $table->foreign('status_id')->references('id')->on('statuses');
+
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('services');
+  }
+}
