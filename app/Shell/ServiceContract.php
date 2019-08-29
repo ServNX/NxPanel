a042a04000d38eab86375695c79a65deaad96f48
poppabear8883 <servnx@gmail.com>
2019-08-29 10:59:28 -0400

    (Add) Shell Services Implementation
    Dependant on Monit to be installed on the system

---
+++ app/Shell/ServiceContract.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Shell;
+
+interface ServiceContract
+{
+    public function start(String $service): bool;
+
+    public function stop(String $service): bool;
+
+    public function restart(String $service): bool;
+
+    public function uid(String $service): string;
+
+    public function gid(String $service): string;
+
+    public function uptime(String $service): string;
+
+    public function cpu(String $service): string;
+
+    public function cpuTotal(String $service): string;
+
+    public function memory(String $service): string;
+
+    public function memoryTotal(String $service): string;
+
+    public function diskRead(String $service): string;
+
+    public function diskWrite(String $service): string;
+
+    public function status(String $service): string;
+
+    public function pid(String $service): string;
+
+    public function parentPid(String $service): string;
+}
